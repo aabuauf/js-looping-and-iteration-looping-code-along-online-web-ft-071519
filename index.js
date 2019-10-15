63f40925485b7983b32558f22d767b6cfc305765
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-15 12:14:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function writeCards(names, eventNames){
   let newarray = [];
   for(let i=0; i < names.length; i++){
-       newarray.push(`Thank you, ${names[i]}, for the wonderful ${eventNames[i]} gift!`) 
+       newarray.push(`Thank you, ${names[i]}, for the wonderful eventNames gift!`) 
 
   }
   
