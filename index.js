3ffeb8df845fdb80a3082de15e677ee8a9361597
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-15 12:11:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 function writeCards(names, eventNames){
   for(let i=0; i < names.length; i++){
         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        "Thank you, Ada, for the wonderful birthday gift!",
   }
   
 }
